9ce0ed86f6467b519c47bef6840380f9cc0d2fc4
Hélder Costa <helderbadim@hotmail.com>
2025-07-30 16:25:28 +0100

    .

--- inc/db-connect.inc.php
+++ inc/db-connect.inc.php
@@ -1,11 +1,16 @@
 <?php
 
-try{
-    $pdo = new PDO('mysql:host=localhost;dbname=diary', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
+$host = 'sql210.epizy.com'; // ou conforme o painel
+$dbname = 'if0_39591042_travel_diary'; // o nome exato da base
+$user = 'if0_39591042';
+$pass = 'pirusas14';
 
-catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    echo "❌ Erro na ligação: " . $e->getMessage();
     die();
 }
-
+?>
